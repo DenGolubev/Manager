69a0523da4a2cd8abe6a4c94412c8c8f97234d36
Денис Голубев <amigo36@yandex.ru>
2021-06-27 13:27:07 +0300

    Изменил БД на MySQL, убрал лишнее OrderCard

--- Сustomers/Customer.cs
+++ Сustomers/Customer.cs
@@ -8,23 +8,27 @@ namespace Manager
         private string name;
         private string tel;
         private string email;
+        private string communication_method;
+        private string comments;
         public string Name { get { return name; } set { name = value; } }
         public string Tel { get { return tel; } set { tel = value; } }
         public string Email { get { return email; } set { email = value; } }
+        public string CommunicationMethod { get {return communication_method; } set { communication_method = value; } }
+        public string Comments { get { return comments; } set { comments = value; } }
 
         public Customer() { }
-
-        public Customer(string name, string tel, string email)
+        public Customer(string name, string tel, string email = null, string communication_method = null, string comments = null)
         {
             this.name = CreateName(name);
             this.tel = CreateTel(tel);
             this.email = CreateEmail(email);
-
+            this.communication_method = communication_method;
+            this.comments = comments;
         }
 
         public override string ToString()
         {
-            return $"Логин {Name}\nТелефон {Tel}\nПочта {Email}";
+            return $"Логин {Name}\nТелефон {Tel}\nПочта {Email}\nСпособ связи{CommunicationMethod}\nКомментарии{Comments}";
         }
     }
 }
